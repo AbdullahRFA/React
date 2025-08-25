b7935db6c802f2505f1b2bb148088daeaf6a416b
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-26 00:53:49 +0600

    create first reat + vite app

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello React</h1>
+      <p>This is Abdullah Nazmus-Sakib</p>
+    </div>
+  ); 
+}
+export default App;
---
+++ my_react_app/src/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h3>This is header</h3>
+    </div>
+  );
+}
+
+export default Header
