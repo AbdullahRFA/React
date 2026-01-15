d32e109f090612b96cf11bad2f4b5e7baed8c493
AbdullahRFA <shakibrybmn@gmail.com>
2026-01-15 12:22:54 +0600

    write first component with function

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -3,11 +3,43 @@ import Header from "./header";
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>Hello, React!</h1>
       <h3>This is a React app</h3>
+      <Fruits></Fruits>
+      <Color />
+
+      <h3>Result of sumOfDigit: {sumOfDigit()}</h3>
     </div>
   );
 }
 
+function Fruits() {
+  return (
+    <div>
+      <ol type="i">
+        <li>Mango</li>
+        <li>Apple</li>
+        <li>Banana</li>
+      </ol>
+    </div>
+  );
+}
+
+function Color() {
+  return (
+    <div>
+      <ol type="a">
+        <li>Red</li>
+        <li>Blue</li>
+        <li>Yellow</li>
+      </ol>
+    </div>
+  );
+}
+
+
+function sumOfDigit(){
+  return 5 + 10;
+}
 export default App;
