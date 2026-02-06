c9a95f903abbea8261577725eb57d71780006449
AbdullahRFA <shakibrybmn@gmail.com>
2026-02-06 11:03:06 +0600

    implement default propes

---
+++ my_react_app/src/features/default_propes.jsx
@@ -0,0 +1,10 @@
+function DefaultProps({name="New User"}) {
+  return (
+      <div>
+      <h1>Default Props</h1>
+      <p>Hi, {name}</p>
+    </div>
+  );
+}
+
+export default DefaultProps;
---
+++ my_react_app/src/features/propes.jsx
@@ -0,0 +1,13 @@
+import DefaultProps from "./default_propes";
+
+function Propes() {
+  return (
+    <div>
+      <h1>Props in React</h1>
+      <DefaultProps name="Abdullah" />
+      <DefaultProps name="sakib"/>
+    </div>
+  );
+}
+
+export default Propes;
